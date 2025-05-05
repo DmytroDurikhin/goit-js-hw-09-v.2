035b394b7c7fd24ba54682b811e601c5f38e0863
Dmytro Durikhin <durihindmitriy@gmail.com>
2025-05-05 22:53:21 +0300

    added gallery

---
+++ src/js/2-form.js
@@ -0,0 +1,30 @@
+// const formData = {
+//     email: "",
+//     message: ""
+// }
+
+// // Ключ для localStorage
+// const STORAGE_KEY = 'feedback-form-state';
+
+
+
+// const form = document.querySelector('.feedback-form');
+
+// const emailInput = form.elements.email;
+// const messageInput = form.elements.message;
+
+// document.addEventListener('DOMContentLoaded', renderPage);
+
+// function renderPage() {
+//     const saveData = localStorage.getItem(STORAGE_KEY);
+//     if (saveData) {
+//         localStorage.parse(saveData);
+//         emailInput.valuel = parsedData.email || "";
+//     messageInput.value = parsedData.message || "";
+     
+    
+//   }
+
+//     }
+
+
