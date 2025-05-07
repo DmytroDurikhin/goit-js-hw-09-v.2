6dcbc7ee63bad50f028544a43b6ca2b28496ca74
Dmytro Durikhin <durihindmitriy@gmail.com>
2025-05-07 22:30:18 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,30 +1,61 @@
-// const formData = {
-//     email: "",
-//     message: ""
-// }
+const formData = {
+    email: "",
+    message: ""
+}
 
-// // Ключ для localStorage
-// const STORAGE_KEY = 'feedback-form-state';
+// Ключ для localStorage
+const STORAGE_KEY = 'feedback-form-state';
 
 
 
-// const form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
-// const emailInput = form.elements.email;
-// const messageInput = form.elements.message;
-
-// document.addEventListener('DOMContentLoaded', renderPage);
-
-// function renderPage() {
-//     const saveData = localStorage.getItem(STORAGE_KEY);
-//     if (saveData) {
-//         localStorage.parse(saveData);
-//         emailInput.valuel = parsedData.email || "";
-//     messageInput.value = parsedData.message || "";
-     
+document.addEventListener('DOMContentLoaded', renderPage);
+form.addEventListener('submit', handleSubmitForm);
+form.addEventListener('input', handleInputForm);
     
-//   }
+    function renderPage() {
+        const saveData = localStorage.getItem(STORAGE_KEY);
 
-//     }
+        if (saveData) {
+            const parsedData = JSON.parse(saveData);
+            form.elements.email.value = parsedData.email || "";
+            form.elements.message.value = parsedData.message || "";
 
+
+            formData.email = form.elements.email.value;
+            formData.message = form.elements.message.value;
+        }
+    }
+    
+   
+   function handleInputForm(evt) {
+       
+       formData.email = form.elements.email.value;
+       formData.message = form.elements.message.value;
+
+       const formJSON = JSON.stringify(formData);
+
+       localStorage.setItem(STORAGE_KEY, formJSON);
+
+       
+   }
+
+
+function handleSubmitForm(evt) {
+    evt.preventDefault();
+
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+
+    if (!email || !message) {
+        return alert("Fill please all fields");
+    }
+
+console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+
+    form.reset();
+}
 
